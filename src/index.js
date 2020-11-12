46b3249c30bf863a0b5fc5804ef374693c543ab7
xyb <xyb@xyb.name>
2020-11-12 06:23:41 -0800

    Switch to @justforfun-click/mathjax

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
-const { mathjax } = require('mathjax-full/js/mathjax');
-const { AsciiMath } = require('mathjax-full/js/input/asciimath');
-const { SVG } = require('mathjax-full/js/output/svg');
-const { RegisterHTMLHandler } = require('mathjax-full/js/handlers/html');
-const { liteAdaptor } = require('mathjax-full/js/adaptors/liteAdaptor');
+const mathJaxPackage = "@justforfun-click/mathjax"
+const { mathjax } = require(`${mathJaxPackage}/js/mathjax`);
+const { AsciiMath } = require(`${mathJaxPackage}/js/input/asciimath`);
+const { SVG } = require(`${mathJaxPackage}/js/output/svg`);
+const { RegisterHTMLHandler } = require(`${mathJaxPackage}/js/handlers/html`);
+const { liteAdaptor } = require(`${mathJaxPackage}/js/adaptors/liteAdaptor`);
 
 const adaptor = liteAdaptor();
 const handler = RegisterHTMLHandler(adaptor);
@@ -11,7 +12,7 @@ const svg = new SVG();
 const html = mathjax.document('', {InputJax: ascii, OutputJax: svg});
 
 module.exports = async function(context, trigger, input, outputInput) {
-    var path = decodeURI(context.req.params['path'] || 'index.html').replace(/\s+/g, '');
+    var path = decodeURI(context.req.params['path'] || 'index.html').replace(/\s+/g, ' ');
     if (path.startsWith('$/')) {
         var svgContent = adaptor.innerHTML(html.convert(path.substr(2)));
         context.res = {
