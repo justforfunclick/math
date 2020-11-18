57b1d8534bdd1face4a13870fa48df4d56494b13
xyb <xyb@xyb.name>
2020-11-18 08:35:15 -0800

    Update to @justforfun-click/mathjax:3.1.2-3. Support LaTeX.

--- README.md
+++ README.md
@@ -1,8 +1,10 @@
 # AsciiMath to SVG
 
-Convert [AsciiMath](http://asciimath.org/) notation to svg online.
+Convert [AsciiMath](http://asciimath.org/) or [LaTeX](https://en.wikibooks.org/wiki/LaTeX) notation to svg online.
 
 Usage:
+
+Convert AsciiMath notation:
 
 ```
 <img src="https://math.justforfun.click/$/<ascii math notation>">
@@ -17,3 +19,20 @@ For example:
 will get:
 
 <p align="center"><img src="https://math.justforfun.click/$/sum_{i=1}^10 a_{i} + b_{i}"></p>
+
+
+Convert LateX notation:
+
+```
+<img src="https://math.justforfun.click/$$/<latex notation>">
+```
+
+For example:
+
+```
+<img src="https://math.justforfun.click/$$/A_{m,n} = \begin{pmatrix} a_{1,1} & a_{1,2} & \cdots & a_{1,n} -- a_{2,1} & a_{2,2} & \cdots & a_{2,n} -- \vdots  & \vdots  & \ddots & \vdots -- a_{m,1} & a_{m,2} & \cdots & a_{m,n} \end{pmatrix}">
+```
+
+will get:
+
+<p align="center"><img src="https://math.justforfun.click/$$/A_{m,n} = \begin{pmatrix} a_{1,1} & a_{1,2} & \cdots & a_{1,n} -- a_{2,1} & a_{2,2} & \cdots & a_{2,n} -- \vdots  & \vdots  & \ddots & \vdots -- a_{m,1} & a_{m,2} & \cdots & a_{m,n} \end{pmatrix}"></p>
--- src/index.js
+++ src/index.js
@@ -1,20 +1,8 @@
-const mathJaxPackage = "@justforfun-click/mathjax"
-const { mathjax } = require(`${mathJaxPackage}/js/mathjax`);
-const { AsciiMath } = require(`${mathJaxPackage}/js/input/asciimath`);
-const { SVG } = require(`${mathJaxPackage}/js/output/svg`);
-const { RegisterHTMLHandler } = require(`${mathJaxPackage}/js/handlers/html`);
-const { liteAdaptor } = require(`${mathJaxPackage}/js/adaptors/liteAdaptor`);
-
-const adaptor = liteAdaptor();
-const handler = RegisterHTMLHandler(adaptor);
-const ascii = new AsciiMath();
-const svg = new SVG();
-const html = mathjax.document('', {InputJax: ascii, OutputJax: svg});
+const { mathToSvg } = require('@justforfun-click/mathjax/js/mathToSvg');
 
 module.exports = async function(context, trigger, input, outputInput) {
-    var path = (context.req.params['path'] || 'index.html').replace(/\s+/g, ' ');
-    if (path.startsWith('$/')) {
-        var svgContent = adaptor.innerHTML(html.convert(path.substr(2)));
+    var svgContent = mathToSvg(context.req.params['path']);
+    if (svgContent) {
         context.res = {
             headers: {
                 'Content-Type': 'image/svg+xml'
