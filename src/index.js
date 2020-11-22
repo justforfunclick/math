e9445b44c5179c280fe33bbe795197189a30611d
xyb <xyb@xyb.name>
2020-11-22 09:30:09 -0800

    Remove azure functions

--- src/index.js
+++ src/index.js
@@ -1,17 +1,23 @@
+const http = require('http');
 const { mathToSvg } = require('@justforfun-click/mathjax/js/mathToSvg');
 
-module.exports = async function(context, trigger, input, outputInput) {
-    var svgContent = mathToSvg(context.req.params['path']);
-    if (svgContent) {
-        context.res = {
-            headers: {
-                'Content-Type': 'image/svg+xml'
-            },
-            body: svgContent
-        };
-    } else {
-        context.res = {
-            status: 404
-        };
+const listener = (req, res) => {
+    var path = decodeURIComponent(req.url);
+    for (var i = 0; i < path.length; ++i) {
+        if (path[i] != '/') {
+            path = path.substr(i);
+            break;
+        }
     }
-}
+    var svgContent = mathToSvg(path);
+    if (svgContent) {
+        res.setHeader("Content-Type", "image/svg+xml");
+        res.end(svgContent);
+    } else {
+        res.writeHead(404);
+        res.end();
+    }
+};
+
+const server = http.createServer(listener);
+server.listen(process.env.PORT || 8080)
